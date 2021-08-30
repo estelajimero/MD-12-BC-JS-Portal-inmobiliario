566ece554ca4457dc777f2df4aeb6a70a550be66
Estela <estelajimero@users.noreply.github.com>
2021-08-30 13:23:04 +0200

    Create constants for menu

---
+++ src/pages/property-list/property-list.constants.js
@@ -0,0 +1,25 @@
+// Definir los arrays para nº de habitaciones, número de baños, etc.
+
+export const roomOptions = [
+    {
+        id: '1',
+        name: '+1',
+    },
+    {
+        id: '2',
+        name: '+2',
+    },
+    {
+        id: '3',
+        name: '+3',
+    },
+    {
+        id: '4',
+        name: '+4',
+    },
+    {
+        id: '5',
+        name: '+5',
+    },
+];
+
