62acb6b9d468c97d0610864c712fd7919592a192
Estela <estelajimero@users.noreply.github.com>
2021-08-31 10:03:57 +0200

    Add filter search mapper

--- src/pages/property-list/property-list.mappers.js
+++ src/pages/property-list/property-list.mappers.js
@@ -27,3 +27,33 @@ export const mapPropertyListFromApiToVM = propertyList => {
 };
 
 const getRoomWord = rooms => rooms > 1 ? 'habitaciones' : 'habitación';
+
+export const mapFilterToQueryParams = filter => {
+    let queryParams = '';
+
+    if(filter.saleTypeId) {
+        queryParams = `${queryParams}saleTypeIds_like=${filter.saleTypeId}&`;
+    }
+
+    if(filter.provinceId) {
+        queryParams = `${queryParams}provinceId=${filter.provinceId}&`;
+    }
+
+    if(filter.minRooms) {
+        queryParams = `${queryParams}rooms_gte=${filter.minRooms}&`; // usamos el & para añadir más parámetros
+    } 
+
+    if(filter.minBathRooms) {
+        queryParams = `${queryParams}bathrooms_gte${filter.minBathRooms}&`; 
+    }
+    
+    if(filter.minPrice) {
+        queryParams = `${queryParams}price_gte${filter.minPrice}&`; 
+    }
+
+    if(filter.maxPrice) {
+        queryParams = `${queryParams}price_lte${filter.maxPrice}&`; 
+    }
+
+    return queryParams.slice(0, -1);
+};
