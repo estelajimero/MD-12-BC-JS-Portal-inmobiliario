f81fd63338eee45b845236df5fa059df41040edc
Estela <estelajimero@users.noreply.github.com>
2021-09-01 11:49:34 +0200

    Add form navigation

--- src/pages/property-detail/property-detail.js
+++ src/pages/property-detail/property-detail.js
@@ -5,9 +5,20 @@
     * Crear método post para enviar información de contacto. */
 
 import { history } from '../../core/router';
-import { getEquipment, getProperty } from './property-detail.api';
+import { 
+    getEquipment, 
+    getProperty, 
+    isValidContact 
+} from './property-detail.api';
 import { setPropertyValues } from './property-detail.helpers';
 import { mapPropertyFromApiToVM } from './property-detail.mappers';
+import { formValidation } from './property-detail.validations';
+import { 
+    onUpdateField, 
+    onSetError, 
+    onSubmitForm, 
+    onSetFormErrors 
+} from '../../common/helpers';
 
 const params = history.getParams();
 
@@ -44,3 +55,55 @@ let form = {
     email: '',
     message: '',
 }
+
+onUpdateField('email', (event) => {
+    const value = event.target.value;
+
+    form = {
+        ...form,
+        email: value
+    };
+
+    formValidation.validateField('email', form.email).then(result => {
+        onSetError('email', result);
+    });
+});
+
+onUpdateField('message', (event) => {
+    const value = event.target.value;
+
+    form = {
+        ...form,
+        message: value
+    };
+
+    formValidation.validateField('message', form.message).then(result => {
+        onSetError('message', result);
+    });
+});
+
+const resetForm = () => {
+    const form = document.getElementsByClassName('form-detalle-propiedad');
+
+    form[0].reset();
+};
+
+const onNavigate = (isValid) => {
+    if (isValid) {
+        alert('Solicitud de contacto realizada correctamente');
+    } else {
+        alert('Error al enviar la solicitud de contacto');
+    }
+};
+
+onSubmitForm('contact-button', ()=> {
+    formValidation.validateForm(form).then(result => {
+        onSetFormErrors(result);
+        if (result.succeeded) {
+            isValidContact(form).then(isValid => {
+                onNavigate(isValid);
+                resetForm();
+            });
+        }
+    });
+});
