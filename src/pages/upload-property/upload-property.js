9bb1c7ab1386a04773cce77de54c64d8d5b198a6
Estela <estelajimero@users.noreply.github.com>
2021-09-01 11:49:49 +0200

    First commit

---
+++ src/pages/upload-property/upload-property.js
@@ -0,0 +1,9 @@
+/** 
+ * Recuperar los valores del formulario de Datos generales.
+ * Crear validaciones necesarias de dicho formulario.
+ * Recuperar los valores del formulario de Datos de la vivienda.
+ * Crear validaciones necesarias de dicho formulario.
+ * Recuperar los valores del formulario de Subir fotos.
+ * Crear mapper para cumplir con el modelo de la api.
+ * Crear método post para enviar información del formulario.
+ */
