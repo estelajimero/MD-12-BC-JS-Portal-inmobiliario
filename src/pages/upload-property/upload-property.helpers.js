194a322a946191a49251bd90c7ef20ee8a009f5c
Estela <estelajimero@users.noreply.github.com>
2021-09-03 11:13:55 +0200

    Add addElement & removeElement

--- src/pages/upload-property/upload-property.helpers.js
+++ src/pages/upload-property/upload-property.helpers.js
@@ -93,3 +93,17 @@ export const onAddImage = image => {
   imageContainerElement.appendChild(imageElement);
   imagesElement.insertBefore(imageContainerElement, addImageButton);
 };
+
+export const addElement = (value, obj, id) => {
+  return {
+      ...obj,
+      [id]: [...obj[id], value],
+  };
+};
+
+export const removeElement = (value, obj, id) => {
+  return {
+    ...obj,
+    [id]: obj[id].filter(element => element !== value)
+  };
+};
